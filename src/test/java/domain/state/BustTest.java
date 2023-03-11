a6071a2a07a7d81efbbb49c2bfe1719313e79419
HubCreator <chocomilk8604@gmail.com>
2023-03-11 19:15:48 +0900

    test : BustTest 테스트 케이스 추가

--- src/test/java/domain/state/BustTest.java
+++ src/test/java/domain/state/BustTest.java
@@ -3,37 +3,38 @@ package domain.state;
 import domain.card.Card;
 import domain.card.CardNumber;
 import domain.card.CardShape;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-class BlackjackTest {
+class BustTest {
 
     private State state;
 
     @BeforeEach
     void init() {
         state = new Ready()
-                .draw(Card.of(CardShape.HEART, CardNumber.of(1)))
-                .draw(Card.of(CardShape.HEART, CardNumber.of(10)));
+                .draw(Card.of(CardShape.CLOVER, CardNumber.of(10)))
+                .draw(Card.of(CardShape.HEART, CardNumber.of(10)))
+                .draw(Card.of(CardShape.DIAMOND, CardNumber.of(10)));
+    }
+    @Test
+    void bustTest() {
+        assertThat(state).isInstanceOf(Bust.class);
     }
 
     @Test
-    void blackjackTest() {
-        assertThat(state).isInstanceOf(Blackjack.class);
-    }
-
-    @Test
-    void blackjackStayTest() {
+    void bustStayTest() {
         assertThatThrownBy(() -> state.stay())
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessage("게임이 끝났습니다.");
     }
 
     @Test
-    void blackjackDrawTest() {
+    void bustDrawTest() {
         assertThatThrownBy(() -> state.draw(Card.of(CardShape.CLOVER, CardNumber.of(5))))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessage("카드를 추가로 받을 수 없습니다.");
